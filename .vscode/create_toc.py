42bc5ff062145ffdbf762070a3fa4ec4de5345b4
lo9ud <58418557+lo9ud@users.noreply.github.com>
2023-03-15 10:40:36 +0200

    implemented ordered toc

--- .vscode/create_toc.py
+++ .vscode/create_toc.py
@@ -1,5 +1,9 @@
 import os
+def section_sort_key(x):
+    return ord(x[0])
 
+def chapter_sort_key(x):
+    return x.startswith("P") and int(x[1])
 
 
 toc = """# Table of contents
@@ -12,21 +16,22 @@ print("Scanning root directory for chapters...")
 folders = {folder.path:{} for folder in os.scandir("./py_tutorial/") if folder.name not in ["_build"] and folder.is_dir()}
 print("Directory scanned.\nScanning for sections...")
 for folder in folders:
-    folders[folder] = {c_folder.path.replace("\\","/"):[] for c_folder in os.scandir(folder) if c_folder.name not in ["_build"] and c_folder.is_dir()}
+    folders[folder] = [c_folder.path.replace("\\","/") for c_folder in os.scandir(folder) if c_folder.name not in ["_build"] and c_folder.is_dir()]
 print("Sections scanned.\nBuilding toc...")
-for folder in folders:
+for folder in sorted(folders.keys(), key=chapter_sort_key):
     print(f"Adding chapter {folder}")
     chapter = f"\n - file: {folder[14:]}/index"
-    if folders[folder].keys():
+    if folders[folder]:
         print("Sections available for chapter, adding sections...")
         chapter += "\n   sections:"
-        for section in folders[folder]:
+        for section in sorted(folders[folder], key = section_sort_key):
             print(f"\tAdding section {section}")
             chapter += f"\n   - file: {section[14:]}/index"
     print("Chapter added to toc")
     toc += chapter
 
-#print(toc)
+print("toc built.")
+print(toc)
 print("Removing old _toc.yml")
 os.remove("./py_tutorial/_toc.yml")
 print("Removed.\nCreating new _toc.yml...")
