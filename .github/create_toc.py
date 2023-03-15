38df05a570644786d6fe7071a7684bfef0d6685b
lo9ud <58418557+lo9ud@users.noreply.github.com>
2023-03-15 17:24:39 +0200

    fixed build_toc.py

--- .github/create_toc.py
+++ .github/create_toc.py
@@ -21,10 +21,10 @@ print("Directory scanned.\nScanning for sections...")
 for folder in folders:
     folders[folder] = [c_folder.path.replace("\\","/") for c_folder in os.scandir(folder) if c_folder.name not in ["_build"] and c_folder.is_dir()] # type: ignore
  
-print("Sections scanned.\nBuilding toc...")
+print("Sections scanned.\nBuilding toc...\n")
 for folder in sorted(folders.keys(), key=chapter_sort_key):
 
-    print(f"Adding chapter {folder}")
+    print(f"Adding chapter ({chapter_sort_key(folder)}){folder}")
     chapter = f"\n - file: {folder[14:]}/index"
 
     if folders[folder]:
@@ -32,14 +32,14 @@ for folder in sorted(folders.keys(), key=chapter_sort_key):
         chapter += "\n   sections:"
 
         for section in sorted(folders[folder], key = section_sort_key):
-            print(f"\tAdding section {section}")
+            print(f"\tAdding section ({section_sort_key(section)}){section}")
             chapter += f"\n   - file: {section[14:]}/index"
             
-    print("Chapter added to toc")
+    print("Chapter added to toc\n")
     toc += chapter
 
 print("toc built.")
-print(toc)
+print(*map(lambda x: ">>> " + x,toc.splitlines(keepends=True)))
 print("Removing old _toc.yml")
 os.remove("./py_tutorial/_toc.yml")
 print("Removed.\nCreating new _toc.yml...")
